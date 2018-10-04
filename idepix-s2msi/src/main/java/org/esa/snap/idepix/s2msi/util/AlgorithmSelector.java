043687b7c91b5ecd0b7d4418a8ac239ec2223dce
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-10-04 17:29:16 +0200

    added s2 idepix to snap-idepix

---
+++ idepix-s2msi/src/main/java/org/esa/snap/idepix/s2msi/util/AlgorithmSelector.java
@@ -0,0 +1,10 @@
+package org.esa.snap.idepix.s2msi.util;
+
+/**
+ * Enumeration for selection of cloud screening algorithm
+ *
+ * @author Olaf Danne
+ */
+public enum AlgorithmSelector {
+    MSI   // we only need this one actually
+}
