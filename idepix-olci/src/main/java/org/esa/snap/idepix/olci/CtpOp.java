0fbafb41f5629258a2c036a62ceef8791d1492a5
Olaf Danne <olaf.danne@brockmann-consult.de>
2019-02-13 17:21:22 +0100

    started with OLCI CTP Op

---
+++ idepix-olci/src/main/java/org/esa/snap/idepix/olci/CtpOp.java
@@ -0,0 +1,176 @@
+package org.esa.snap.idepix.olci;
+
+import org.esa.s3tbx.idepix.core.AlgorithmSelector;
+import org.esa.s3tbx.idepix.core.IdepixConstants;
+import org.esa.s3tbx.idepix.core.operators.BasisOp;
+import org.esa.s3tbx.idepix.core.util.IdepixIO;
+import org.esa.snap.core.datamodel.Band;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.core.gpf.GPF;
+import org.esa.snap.core.gpf.OperatorException;
+import org.esa.snap.core.gpf.OperatorSpi;
+import org.esa.snap.core.gpf.annotations.OperatorMetadata;
+import org.esa.snap.core.gpf.annotations.Parameter;
+import org.esa.snap.core.gpf.annotations.SourceProduct;
+import org.esa.snap.core.gpf.annotations.TargetProduct;
+import org.esa.snap.core.util.ProductUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * The Idepix pixel classification operator for OLCI products.
+ * <p>
+ * Initial implementation:
+ * - pure neural net approach which uses MERIS heritage bands only
+ * - no auxdata available yet (such as 'L2 auxdata' for MERIS)
+ * <p>
+ * Currently resulting limitations:
+ * - no cloud shadow flag
+ * - glint flag over water just taken from 'sun_glint_risk' in L1 'quality_flags' band
+ * <p>
+ * Advanced algorithm to be defined which makes use of more bands.
+ *
+ * @author olafd
+ */
+@OperatorMetadata(alias = "Snap.Idepix.Olci",
+        category = "Optical/Pre-Processing",
+        version = "1.0",
+        authors = "Olaf Danne",
+        copyright = "(c) 2018 by Brockmann Consult",
+        description = "Pixel identification and classification for OLCI.")
+public class CtpOp extends BasisOp {
+
+    @SourceProduct(alias = "sourceProduct",
+            label = "OLCI L1b product",
+            description = "The OLCI L1b source product.")
+    private Product sourceProduct;
+
+    @TargetProduct(description = "The target product.")
+    private Product targetProduct;
+
+    private boolean outputRadiance;
+    private boolean outputRad2Refl;
+
+    @Parameter(description = "The list of radiance bands to write to target product.",
+            label = "Select TOA radiances to write to the target product",
+            valueSet = {
+                    "Oa01_radiance", "Oa02_radiance", "Oa03_radiance", "Oa04_radiance", "Oa05_radiance",
+                    "Oa06_radiance", "Oa07_radiance", "Oa08_radiance", "Oa09_radiance", "Oa10_radiance",
+                    "Oa11_radiance", "Oa12_radiance", "Oa13_radiance", "Oa14_radiance", "Oa15_radiance",
+                    "Oa16_radiance", "Oa17_radiance", "Oa18_radiance", "Oa19_radiance", "Oa20_radiance",
+                    "Oa21_radiance"
+            },
+            defaultValue = "")
+    private String[] radianceBandsToCopy;
+
+    @Parameter(description = "The list of reflectance bands to write to target product.",
+            label = "Select TOA reflectances to write to the target product",
+            valueSet = {
+                    "Oa01_reflectance", "Oa02_reflectance", "Oa03_reflectance", "Oa04_reflectance", "Oa05_reflectance",
+                    "Oa06_reflectance", "Oa07_reflectance", "Oa08_reflectance", "Oa09_reflectance", "Oa10_reflectance",
+                    "Oa11_reflectance", "Oa12_reflectance", "Oa13_reflectance", "Oa14_reflectance", "Oa15_reflectance",
+                    "Oa16_reflectance", "Oa17_reflectance", "Oa18_reflectance", "Oa19_reflectance", "Oa20_reflectance",
+                    "Oa21_reflectance"
+            },
+            defaultValue = "")
+    private String[] reflBandsToCopy;
+
+    @Parameter(defaultValue = "false",
+            label = " Write NN value to the target product",
+            description = " If applied, write NN value to the target product ")
+    private boolean outputSchillerNNValue;
+
+    @Parameter(defaultValue = "true", label = " Compute a cloud buffer")
+    private boolean computeCloudBuffer;
+
+    @Parameter(defaultValue = "2", interval = "[0,100]",
+            description = "The width of a cloud 'safety buffer' around a pixel which was classified as cloudy.",
+            label = "Width of cloud buffer (# of pixels)")
+    private int cloudBufferWidth;
+
+    @Parameter(defaultValue = "false",
+            label = " Use SRTM Land/Water mask",
+            description = "If selected, SRTM Land/Water mask is used instead of L1b land flag. " +
+                    "Slower, but in general more precise.")
+    private boolean useSrtmLandWaterMask;
+
+
+    private Product rad2reflProduct;
+    private Product o2CorrProduct;
+
+    @Override
+    public void initialize() throws OperatorException {
+
+        final boolean inputProductIsValid = IdepixIO.validateInputProduct(sourceProduct, AlgorithmSelector.OLCI);
+        if (!inputProductIsValid) {
+            throw new OperatorException(IdepixConstants.INPUT_INCONSISTENCY_ERROR_MESSAGE);
+        }
+
+        preProcess();
+
+        Product ctpProduct = computeCtpProduct();
+        ctpProduct.setName(sourceProduct.getName() + "_IDEPIX");
+        ctpProduct.setAutoGrouping("Oa*_radiance:Oa*_reflectance");
+
+        ProductUtils.copyFlagBands(sourceProduct, ctpProduct, true);
+
+
+
+        targetProduct = createTargetProduct(ctpProduct);
+        targetProduct.setAutoGrouping(ctpProduct.getAutoGrouping());
+
+    }
+
+    private Product computeCtpProduct() {
+        // todo
+        return null;
+    }
+
+    private Product createTargetProduct(Product idepixProduct) {
+        Product targetProduct = new Product(idepixProduct.getName(),
+                                            idepixProduct.getProductType(),
+                                            idepixProduct.getSceneRasterWidth(),
+                                            idepixProduct.getSceneRasterHeight());
+
+        ProductUtils.copyMetadata(idepixProduct, targetProduct);
+        ProductUtils.copyGeoCoding(idepixProduct, targetProduct);
+        ProductUtils.copyFlagCodings(idepixProduct, targetProduct);
+        ProductUtils.copyFlagBands(idepixProduct, targetProduct, true);
+        ProductUtils.copyMasks(idepixProduct, targetProduct);
+        ProductUtils.copyTiePointGrids(idepixProduct, targetProduct);
+        targetProduct.setStartTime(idepixProduct.getStartTime());
+        targetProduct.setEndTime(idepixProduct.getEndTime());
+
+        IdepixOlciUtils.setupOlciClassifBitmask(targetProduct);
+
+
+        if (outputSchillerNNValue) {
+            ProductUtils.copyBand(IdepixConstants.NN_OUTPUT_BAND_NAME, idepixProduct, targetProduct, true);
+        }
+
+        return targetProduct;
+    }
+
+
+    private void preProcess() {
+        rad2reflProduct = IdepixOlciUtils.computeRadiance2ReflectanceProduct(sourceProduct);
+        Map<String, Product> o2corrSourceProducts = new HashMap<>();
+        Map<String, Object> o2corrParms = new HashMap<>();
+        o2corrParms.put("processOnlyBand13", false);
+        o2corrSourceProducts.put("l1bProduct", sourceProduct);
+        final String o2CorrOpName = "O2CorrOlci";
+        o2CorrProduct = GPF.createProduct(o2CorrOpName, o2corrParms, o2corrSourceProducts);
+    }
+
+    /**
+     * The Service Provider Interface (SPI) for the operator.
+     * It provides operator meta-data and is a factory for new operator instances.
+     */
+    public static class Spi extends OperatorSpi {
+
+        public Spi() {
+            super(CtpOp.class);
+        }
+    }
+}
