3c51b054b261898bcbb97b32d8e15fb3e5515438
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-05-14 14:52:52 +0200

    extracted idepix core

---
+++ idepix-core/src/main/java/org/esa/snap/idepix/core/AlgorithmSelector.java
@@ -0,0 +1,20 @@
+package org.esa.snap.idepix.core;
+
+/**
+ * Enumeration for selection of cloud screening algorithm
+ *
+ * @author Olaf Danne
+ */
+public enum AlgorithmSelector {
+    AVHRR,
+    LANDSAT8,
+    MODIS,
+    MSI,
+    PROBAV,
+    SEAWIFS,
+    VIIRS,
+    MERIS,
+    OLCI,
+    OLCISLSTR,
+    VGT
+}
