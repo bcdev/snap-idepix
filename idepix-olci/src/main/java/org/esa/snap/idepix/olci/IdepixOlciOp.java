3a5e1aefdf5ed8b81f77df0eeb4f58a395dfa655
martin-boettcher <martin.boettcher@brockmann-consult.de>
2019-08-12 08:39:43 +0200

    modified local tile cache size for one product, protected local tile cache with property snap.gpf.disableTileCache
    (cherry picked from commit a06bb30)

--- idepix-olci/src/main/java/org/esa/snap/idepix/olci/IdepixOlciOp.java
+++ idepix-olci/src/main/java/org/esa/snap/idepix/olci/IdepixOlciOp.java
@@ -177,7 +177,7 @@ public class IdepixOlciOp extends BasisOp {
                         olciIdepixProduct,
                         alternativeNNDirPath,
                         outputCtp);
-                ctpProduct = computeTileCacheProduct(ctpProduct,72);
+                ctpProduct = computeTileCacheProduct(ctpProduct,76);
             }
 
             postProcess(olciIdepixProduct);
@@ -260,11 +260,13 @@ public class IdepixOlciOp extends BasisOp {
     }
 
     private Product computeTileCacheProduct(Product inputProduct,int cacheSize) {
-        TileCacheOp tileCacheOp = new TileCacheOp();
-        tileCacheOp.setSourceProduct("source",inputProduct);
-        tileCacheOp.setParameterDefaultValues();
-        tileCacheOp.setParameter("cacheSize",cacheSize);
-        inputProduct = tileCacheOp.getTargetProduct();
+        if (Boolean.getBoolean("snap.gpf.disableTileCache")) {
+            TileCacheOp tileCacheOp = new TileCacheOp();
+            tileCacheOp.setSourceProduct("source", inputProduct);
+            tileCacheOp.setParameterDefaultValues();
+            tileCacheOp.setParameter("cacheSize", cacheSize);
+            inputProduct = tileCacheOp.getTargetProduct();
+        }
         return inputProduct;
     }
 
