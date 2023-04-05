3b2fabcb51d79101864ca606d83ec6a988b8715a
martin-boettcher <martin.boettcher@brockmann-consult.de>
2023-04-05 22:10:00 +0200

    implementation of CTP with a single call to Tensorflow

--- idepix-c3solcislstr/src/main/java/org/esa/snap/idepix/c3solcislstr/C3SOlciSlstrCtpOp.java
+++ idepix-c3solcislstr/src/main/java/org/esa/snap/idepix/c3solcislstr/C3SOlciSlstrCtpOp.java
@@ -199,9 +199,9 @@ public class C3SOlciSlstrCtpOp extends BasisOp {
         for (int y = targetRectangle.y; y < targetRectangle.y + targetRectangle.height; y++) {
             checkForCancellation();
             for (int x = targetRectangle.x; x < targetRectangle.x + targetRectangle.width; x++) {
-                final boolean pixelIsValid = !l1FlagsTile.getSampleBit(x, y, IdepixOlciConstants.L1_F_INVALID);
+                final boolean pixelIsValid = !l1FlagsTile.getSampleBit(x, y, C3SOlciSlstrConstants.L1_F_INVALID);
                 if (pixelIsValid) {
-                    targetTile.setSample(x, y, TensorflowNNCalculator.convertNNResultToCtp(
+                    targetTile.setSample(x, y, C3SOlciSlstrTensorflowNNCalculator.convertNNResultToCtp(
                             nnResult[(y-targetRectangle.y) * targetRectangle.width + (x-targetRectangle.x)][0]));
                 } else {
                     targetTile.setSample(x, y, Float.NaN);
